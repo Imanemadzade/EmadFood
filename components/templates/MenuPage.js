5daf3273a9a037a0199cc6a944c17089c71b6530
Iman-emadzade <imanemadzade0@gmail.com>
2026-01-28 17:03:18 +0330

    feat: menu Route and Menu Page (SSG page) are added to project

---
+++ components/templates/MenuPage.js
@@ -0,0 +1,7 @@
+import styles from "./MenuPage.module.css";
+function MenuPage({ data }) {
+  console.log(data);
+  return <div>MenuPage</div>;
+}
+
+export default MenuPage;
---
+++ pages/menu/index.js
@@ -0,0 +1,18 @@
+import MenuPage from "../../components/templates/MenuPage";
+
+function Menu({ data }) {
+  return <MenuPage data={data} />;
+}
+
+export default Menu;
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3001/data");
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: { data },
+    revalidate: 10, //seconds
+  };
+}
