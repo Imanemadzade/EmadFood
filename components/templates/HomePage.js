f76fbbc94b959c12e44b8e8b5d520db1efc9923c
Iman-emadzade <imanemadzade0@gmail.com>
2026-01-28 12:05:16 +0330

    feat: Definition was build and add to HomePage

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,12 +1,14 @@
 import styles from "./HomePage.module.css";
 import Banner from "../modules/Banner";
 import Attributes from "../modules/Attributes";
+import Definition from "../modules/Definition";
 
 function HomePage() {
   return (
     <div className={styles.container}>
       <Banner />
       <Attributes />
+      <Definition />
     </div>
   );
 }
